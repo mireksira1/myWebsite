0e689415f0b6d4ea467231dc3e9923b74fefda86
Mirek <115916534+mireksira1@users.noreply.github.com>
2022-12-29 14:37:25 +0100

    .

---
+++ matematika/assets/seznam_zaku.js
@@ -0,0 +1,14 @@
+// Nacteni json souboru s jmeny
+fetch('/matematika/assets/zaci.json')
+    .then(response => response.json())
+    .then(data => {
+        // Ziskani lokace pro vlozeni
+        const selectElement = document.querySelector('#listjmen');
+        // Projde data z json a vlozi je do listu
+        data.zaci.forEach(item => {
+            const optionElement = document.createElement('option');
+            optionElement.value = item.id;
+            optionElement.textContent = item.jmeno;
+            selectElement.appendChild(optionElement);
+        });
+    });
